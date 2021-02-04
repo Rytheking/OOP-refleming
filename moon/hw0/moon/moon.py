5c3ad4bd363d44766b7d8a69adf6ea4656338d3b
rytheking <ryanelliottfleming@gmail.com>
2021-02-04 07:40:42 -0800

    Add object Example

---
+++ moon/hw0/moon/moon.py
@@ -0,0 +1,28 @@
+class Moon:
+
+    #constructor
+    def __init__(self,size : int,color : str, phase : int) -> None:
+
+        #properties
+        self._size :int = size
+        self._color : str = color
+        self._phase : int = 0
+
+    def isNewMoon(self) -> bool:
+        return self._phase == 0
+        
+    def cycle(self) -> None:
+        self._phase = (self._phase +1)%4
+    
+    @property
+    def phase(self):
+        return self._phase
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @property
+    def size(self):
+        return self._size
+    
