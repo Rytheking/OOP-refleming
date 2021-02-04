a4d90be698f4834fd6b9d26bb2e8db8356e03769
rytheking <ryanelliottfleming@gmail.com>
2021-02-04 07:49:51 -0800

    Add my own object for hw1

---
+++ moon/hw1/france.py
@@ -0,0 +1,17 @@
+import random
+
+class France:
+    a = "republic"
+    b = "monarchy"
+    c = "facist dictatorship"
+
+    def __init__(self,govtype : str,year: int, head : str, population: int) -> None:
+        self._govtype : str = govtype
+        self._year : int = year
+        self._head : str = head
+        self._population : str = population
+    
+    def revolution()
+    {
+        a :int = randrange(4)
+    }
