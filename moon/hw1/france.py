e472433de15cb6f48a6b0d2d751e95a3ce1454e8
rytheking <ryanelliottfleming@gmail.com>
2021-02-04 11:36:25 -0800

    Add revolution method, change govtype and head to conform

--- moon/hw1/france.py
+++ moon/hw1/france.py
@@ -1,10 +1,12 @@
 import random
 
 class France:
-    a = "republic"
-    b = "monarchy"
-    c = "facist dictatorship"
-
+    r = "republic"
+    m = "monarchy"
+    f = "facist dictatorship"
+    em = "Emmanuel Macron"
+    mlp = "Marie la Pen"
+    lxxb = "Louis XX Bourbon"
     def __init__(self,govtype : str,year: int, head : str, population: int) -> None:
         self._govtype : str = govtype
         self._year : int = year
@@ -13,5 +15,24 @@ class France:
     
     def revolution()
     {
-        a :int = randrange(4)
+        a :int = randrange(3)
+        if(a==1)
+        {
+            self._govtype : str = monarchy
+            self._head : str = lxxb
+        }
+        else if(a==2)
+        {
+            self._govtype : str = f
+            self._head : str = mlp
+        }
+        else if(a==3)
+        {
+            self._govtype :str = r
+            self._head : str = em
+        }
     }
+
+    @property
+        def phase(self):
+        return self._phase
